4c286307a946a80eff4d0635b4329ff10d518ba4
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2024-11-26 00:44:39 +0100

    player can throw dice
    player can press throw dice to get a dice between 1-6 and it will also get displayed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const playerNameForm = document.querySelector('#playerNameForm');
 const nameInput = document.querySelector('#nameInput');
 const playerNameH2 = document.querySelector('#playerName');
+const throwDiceBtn = document.querySelector('#throwDice');
 
 playerNameForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -9,4 +10,28 @@ playerNameForm.addEventListener('submit', event => {
     playerNameForm.remove();
     
     playerNameH2.textContent = `Welcome, ${playerName}!`;
+    playerNameH2.classList.remove('hidden');
+    throwDiceBtn.classList.remove('hidden');
 });
+
+const diceArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
+
+throwDiceBtn.addEventListener('click', event => {
+    const dice = document.createElement('div');
+
+    const diceNumber = getDiceNumber();
+
+    dice.classList.add('die', diceArray[diceNumber]);
+    console.log(dice.classList);
+    console.log(diceNumber);
+
+    for(let i=0; i<diceNumber; i++) {
+        const dieDot = document.createElement('div');
+        dice.append(dieDot);
+    }
+    document.body.append(dice);
+})
+
+function getDiceNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+}
